cb3d9534f3839f83870ffbb02866cded12abd24e
xirothedev <lethanhtrung.trungle@gmail.com>
2025-07-29 01:25:32 +0700

    chore: add initial project configuration and structure
    - Introduced .dockerignore, .editorconfig, .npmrc, and .prettierignore for better development environment setup.
    - Created Dockerfile and docker-compose configuration for containerized application deployment.
    - Added ESLint and Prettier configurations for code quality and formatting.
    - Established initial Storybook setup for UI component development.
    - Updated package.json with new scripts and dependencies for enhanced functionality.

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+	...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+	...storybook.configs["flat/recommended"],
+];
+
+export default eslintConfig;
